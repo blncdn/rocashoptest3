b52ed4a6942cf33e5079fc15b908eb6b0ed5664a
blncdn <67115892+blncdn@users.noreply.github.com>
2023-12-13 19:37:16 +0100

    Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -44,7 +44,7 @@ const Header = () => {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <SearchBox />
+              
               <LinkContainer to='/cart'>
                 <Nav.Link>
                   <FaShoppingCart /> Warenkorb
