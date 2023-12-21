961dad994697aef69ccbc1707d4ce67bc9f50a64
Gregor Glu <67115892+blncdn@users.noreply.github.com>
2023-12-21 23:56:14 +0100

    Add files via upload

---
+++ frontend/src/screens/ImpressumScreen.jsx
@@ -0,0 +1,36 @@
+import {Container, Row, Col } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+const ImpressumScreen = () => {
+
+
+
+  return (
+
+    <Container class="impressum">
+    <Row>  
+      <Col class="impressum" className='text-center py-3'>
+        <h4>
+            IMPRESSUM
+            
+        </h4><br></br><br></br><br></br>
+        <h6>
+            Grigori Gluschtschenko<br></br><br></br>
+            Hebelstrasse 1a <br></br>
+            79379 Müllheim Baden<br></br><br></br>
+
+            Kontakt:<br></br>
+
+            E-Mail: roca@blancdani.com<br></br>
+            Telefon: Auf Anfrage<br></br><br></br><br></br>
+            Umsatzsteuer:<br></br>
+            DE 350 284 467<br></br>
+        </h6>
+      </Col>
+    </Row>
+  </Container>
+
+  );
+};
+
+export default ImpressumScreen;
