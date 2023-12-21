979ad093e078ba6998982ed9dbbc55ba239df8bb
Gregor Glu <67115892+blncdn@users.noreply.github.com>
2023-12-22 00:42:58 +0100

    Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from 'react-bootstrap';
-
+import { FaInstagram } from 'react-icons/fa';
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
@@ -8,6 +8,7 @@ const Footer = () => {
       <Container>
         <Row>  
           <Col className='text-center py-3'>
+          <a class="NavInsta" href="http://instagram.com/rocarokana"Instagram><FaInstagram size=""/> </a>
             <ul class="instLink">
               <a href="/impressum">Impressum
               </a>
