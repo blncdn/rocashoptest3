0857f10cb986cff6afb5403625d285d6870c0c9e
Gregor Glu <67115892+blncdn@users.noreply.github.com>
2023-12-27 00:33:19 +0100

    ProductScreen.jsx

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -82,15 +82,12 @@ const ProductScreen = () => {
           <Meta title={product.name} description={product.description} />
           <Row>
 
-            <Carousel interval={null} class="PCarousel" pause='hover' className='bg-primary mb-4'>           
-              <Carousel.Item class="KItem" key={product._id}>         
+            <Carousel interval={null} pause='hover' className='bg-primary mb-4 PCarousel'>           
+              <Carousel.Item  class="KItem" key={product._id}>         
                 <Image src={product.image[0]} alt={product.name} fluid />         
               </Carousel.Item>
        <Carousel.Item key={product._id}>         
          <Image src={product.image[1]} alt={product.name} fluid /> 
-       </Carousel.Item>
-              <Carousel.Item key={product._id}>         
-         <Image src={product.image[2]} alt={product.name} fluid /> 
        </Carousel.Item>
             </Carousel>
 
