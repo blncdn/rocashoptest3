3f24f56f8cd451c935aaa70b5c8761a8b81fbc41
Gregor Glu <67115892+blncdn@users.noreply.github.com>
2023-12-27 20:31:33 +0100

    ProductScreen.jsx

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -200,11 +200,11 @@ const ProductScreen = () => {
                           onChange={(e) => setRating(e.target.value)}
                         >
                           <option value=''>Wähle...</option>
-                          <option value='1'>1 - Poor</option>
-                          <option value='2'>2 - Fair</option>
-                          <option value='3'>3 - Good</option>
-                          <option value='4'>4 - Very Good</option>
-                          <option value='5'>5 - Excellent</option>
+                          <option value='1'>1 - Stern</option>
+                          <option value='2'>2 - Sterne</option>
+                          <option value='3'>3 - Sterne</option>
+                          <option value='4'>4 - Sterne</option>
+                          <option value='5'>5 - Sterne</option>
                         </Form.Control>
                       </Form.Group>
                       <Form.Group className='my-2' controlId='comment'>
