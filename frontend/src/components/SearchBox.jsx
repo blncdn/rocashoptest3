f446365169500656c3d2a59095cbc755bd99763c
Gregor Glu <67115892+blncdn@users.noreply.github.com>
2023-12-30 00:37:07 +0100

    SearchBox.jsx

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -31,7 +31,7 @@ const SearchBox = () => {
         placeholder='Gold, Silber...'
         className='mr-sm-2 ml-sm-5'
       ></Form.Control>
-      <Button style={{fontSize:'12px'}} type='submit' variant='outline-light' className='p-2 mx-2'>
+      <Button style={{fontSize:'12px'}} type='submit' variant='outline-light' className='p-2 mx-2 searchbtn'>
         Suchen
       </Button>
     </Form>
