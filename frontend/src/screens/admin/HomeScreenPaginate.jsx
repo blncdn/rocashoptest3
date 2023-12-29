31a98f79af4c6db1880bc427ecaac405da9f2d53
Gregor Glu <67115892+blncdn@users.noreply.github.com>
2023-12-29 03:12:08 +0100

    HomeScreenPaginate.jsx

---
+++ frontend/src/screens/admin/HomeScreenPaginate.jsx
@@ -0,0 +1,62 @@
+import { Row, Col, Container } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
+import { Link } from 'react-router-dom';
+import Product from '../components/Product';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import Paginate from '../components/Paginate';
+import ProductCarousel from '../components/ProductCarousel';
+import Meta from '../components/Meta';
+import SearchBox from '../components/SearchBox';
+const HomeScreen = () => {
+  const { pageNumber, keyword } = useParams();
+
+  const { data, isLoading, error } = useGetProductsQuery({
+    keyword,
+    pageNumber,
+  });
+
+  return (
+    <>
+   
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <>
+          <Meta />
+          <br></br>
+          <br></br>
+          <br></br>
+            <div style={{display:'flex', justifyContent:'space-between', height:'38px'}}>
+              <h1 style={{paddingLeft:'10px'}}>Seite {data.page}</h1>
+              <SearchBox/>
+            </div>
+            <br></br>
+   
+          <Container>
+          <Row>
+          
+            {data.products.map((product) => (
+              <Col key={product._id} xs={6} sm={6} md={4} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row></Container>
+          <br></br><br></br><br></br>
+          <Paginate 
+            pages={data.pages}
+            page={data.page}
+            keyword={keyword ? keyword : ''}
+          /><br></br><br></br><br></br>
+        </>
+      )}
+    </>
+  );
+};
+
+export default HomeScreen;
