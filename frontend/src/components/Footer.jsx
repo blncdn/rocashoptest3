87a5d027fe080ec6cb3c30a493b5da08adbc60d7
Gregor Glu <67115892+blncdn@users.noreply.github.com>
2023-12-29 01:02:26 +0100

    Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from 'react-bootstrap';
-import { FaInstagram } from 'react-icons/fa';
+import { FaCcPaypal, FaInstagram, FaCcVisa, FaCcMastercard } from 'react-icons/fa';
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
@@ -7,13 +7,20 @@ const Footer = () => {
     <footer>
       <Container>
         <Row class="RowInsta">   <a class="NavInsta" href="http://instagram.com/rocarokana"Instagram><FaInstagram size=""/> </a>
+          
           <Col className='text-center py-3'>
             <ul class="instLink">
               <a href="/impressum">Impressum
               </a>
             </ul> 
-            <p>ROCA &copy; {currentYear}</p>
+            <p style={{margin:'0'}}>ROCA &copy; {currentYear}</p>
+            <div style={{display:'flex', justifyContent:'center', marginBottom:'0px', }}>
+            <a style={{fontSize:'35px', color:'white',paddingRight:'10px', paddingLeft:'10px'}} class="NavInsta" ><FaCcPaypal size=""/> </a>
+            <a style={{fontSize:'35px', color:'white', paddingRight:'10px', paddingLeft:'10px'}} class="NavInsta" ><FaCcVisa size=""/> </a>
+            <a style={{fontSize:'35px', color:'white', paddingRight:'10px', paddingLeft:'10px'}} class="NavInsta" ><FaCcMastercard size=""/> </a>
+          </div>
           </Col>
+          
         </Row>
       </Container>
     </footer>
