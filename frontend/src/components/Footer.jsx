771d9b34817cc0010feec2e8aacc9eb1445dc3fb
Gregor Glu <67115892+blncdn@users.noreply.github.com>
2023-12-29 02:51:37 +0100

    Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from 'react-bootstrap';
-import { FaCcPaypal, FaInstagram, FaCcVisa, FaCcMastercard } from 'react-icons/fa';
+import { FaCcPaypal, FaInstagram, FaCcVisa, FaCcMastercard,FaCreditCard } from 'react-icons/fa';
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
@@ -7,16 +7,18 @@ const Footer = () => {
     <footer>
       <Container>
         <Row class="RowInsta">   <a class="NavInsta" href="http://instagram.com/rocarokana"Instagram><FaInstagram size=""/> </a>
-          <div style={{display:'flex', justifyContent:'center', marginBottom:'0px', }}>
-            <a style={{fontSize:'35px', color:'white',paddingRight:'10px', paddingLeft:'10px'}} class="NavInsta" ><FaCcPaypal size=""/> </a>
-            <a style={{fontSize:'35px', color:'white', paddingRight:'10px', paddingLeft:'10px'}} class="NavInsta" ><FaCcVisa size=""/> </a>
-            <a style={{fontSize:'35px', color:'white', paddingRight:'10px', paddingLeft:'10px'}} class="NavInsta" ><FaCcMastercard size=""/> </a>
-          </div>
+          
           <Col className='text-center py-3'>
             <ul class="instLink">
               <a href="/impressum">Impressum
               </a>
             </ul> 
+            <div style={{display:'flex', justifyContent:'center', marginBottom:'0px', }}>
+            <a style={{fontSize:'35px', color:'white',paddingRight:'10px', paddingLeft:'10px'}} class="NavInsta" ><FaCcPaypal size=""/> </a>
+            <a style={{fontSize:'35px', color:'white', paddingRight:'10px', paddingLeft:'10px'}} class="NavInsta" ><FaCcVisa size=""/> </a>
+            <a style={{fontSize:'35px', color:'white', paddingRight:'10px', paddingLeft:'10px'}} class="NavInsta" ><FaCcMastercard size=""/> </a>
+            <a style={{fontSize:'35px', color:'white', paddingRight:'10px', paddingLeft:'10px'}} class="NavInsta" ><FaCreditCard size=""/> </a>
+          </div>
             <p style={{margin:'0'}}>ROCA &copy; {currentYear}</p>
             
           </Col>
