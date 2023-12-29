0907a7bfa38efa22e2228f5ec92c48098a061d04
Gregor Glu <67115892+blncdn@users.noreply.github.com>
2023-12-29 01:04:41 +0100

    Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,18 +7,18 @@ const Footer = () => {
     <footer>
       <Container>
         <Row class="RowInsta">   <a class="NavInsta" href="http://instagram.com/rocarokana"Instagram><FaInstagram size=""/> </a>
-          
+          <div style={{display:'flex', justifyContent:'center', marginBottom:'0px', }}>
+            <a style={{fontSize:'35px', color:'white',paddingRight:'10px', paddingLeft:'10px'}} class="NavInsta" ><FaCcPaypal size=""/> </a>
+            <a style={{fontSize:'35px', color:'white', paddingRight:'10px', paddingLeft:'10px'}} class="NavInsta" ><FaCcVisa size=""/> </a>
+            <a style={{fontSize:'35px', color:'white', paddingRight:'10px', paddingLeft:'10px'}} class="NavInsta" ><FaCcMastercard size=""/> </a>
+          </div>
           <Col className='text-center py-3'>
             <ul class="instLink">
               <a href="/impressum">Impressum
               </a>
             </ul> 
             <p style={{margin:'0'}}>ROCA &copy; {currentYear}</p>
-            <div style={{display:'flex', justifyContent:'center', marginBottom:'0px', }}>
-            <a style={{fontSize:'35px', color:'white',paddingRight:'10px', paddingLeft:'10px'}} class="NavInsta" ><FaCcPaypal size=""/> </a>
-            <a style={{fontSize:'35px', color:'white', paddingRight:'10px', paddingLeft:'10px'}} class="NavInsta" ><FaCcVisa size=""/> </a>
-            <a style={{fontSize:'35px', color:'white', paddingRight:'10px', paddingLeft:'10px'}} class="NavInsta" ><FaCcMastercard size=""/> </a>
-          </div>
+            
           </Col>
           
         </Row>
